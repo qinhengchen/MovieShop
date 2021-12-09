99d34276a0e11790bf0f1486cb4c854615237e4a
Wang <Wang@DESKTOP-N52D82Q>
2021-12-09 14:46:38 -0500

    Add project files.

---
+++ Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,45 @@
+﻿using ApplicationCore.RepositoryInterfaces;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T: class
+    {
+        protected readonly MovieShopDbContext _dbContext;
+        public Repository(MovieShopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public T Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            return _dbContext.Set<T>().ToList();
+     
+        }
+
+        public virtual T GetById(int id)
+        {
+            var entity = _dbContext.Set<T>().Find(id);
+            return entity;
+        }
+
+        public T Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
